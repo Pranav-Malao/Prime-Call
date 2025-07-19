8581fd4d65b819aad30983fd97131959cbb9f56d
Pranav-Malao <pranavmalao909@gmail.com>
2025-07-19 17:19:30 +0530

    add final streams sending logic

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -8,7 +8,7 @@ function Room() {
 
   const [remoteSocketId, setRemoteSocketId] = useState(null);
   const [myStream, setMyStream] = useState();
-  // const [remoteStream, setRemoteStream] = useState();
+  const [remoteStream, setRemoteStream] = useState();
 
   const handleUserJoined = useCallback(({ name, id }) => {
     console.log(`User ${name} joined room`);
@@ -35,39 +35,104 @@ function Room() {
       });
       setMyStream(stream);
       const ans = await peer.getAnswer(offer);
-      socket.emit("call:accepted", { to: from, ans });
+      socket.emit("call-accepted", { to: from, ans });
     },
     [socket]
   );
 
-  const myVideoRef = useRef(null);
+  const sendStreams = useCallback(() => {
+    try {
+      if (!myStream) return;
+
+      let alertShown = false; 
+
+      for (const track of myStream.getTracks()) {
+        if (!peer.peer.getSenders().some(sender => sender.track === track)) {
+          peer.peer.addTrack(track, myStream);
+        } else {
+          if (!alertShown) { 
+            console.log("The track has already been added to the peer connection");
+            window.alert("The track has already been added to the peer connection");
+            alertShown = true; 
+          }
+        }
+      }
+    } catch (err) {
+      console.log("Error while adding Track", err);
+    }
+  }, [myStream, peer.peer]);
+
+  const handleCallAccepted = useCallback(
+    ({ from, ans }) => {
+      peer.setLocalDescription(ans);
+      console.log("Call Accepted!");
+      sendStreams();
+    },
+    [sendStreams]
+  );
+
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer-nego-needed", { offer, to: remoteSocketId });
+  }, [remoteSocketId, socket]);
+
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+    };
+  }, [handleNegoNeeded]);
+
+  const handleNegoNeedIncomming = useCallback(
+    async ({ from, offer }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer-nego-done", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleNegoNeedFinal = useCallback(async ({ ans }) => {
+    await peer.setLocalDescription(ans);
+  }, []);
+
+  useEffect(() => {
+    peer.peer.addEventListener("track", async (ev) => {
+      const remoteStream = ev.streams;
+      console.log("GOT TRACKS!!");
+      setRemoteStream(remoteStream[0]);
+    });
+  }, []);
+
 
   useEffect(() => {
     socket.on('user-joined', handleUserJoined);
     socket.on('incoming-call', handleIncommingCall);
+    socket.on("call-accepted", handleCallAccepted);
+    socket.on("peer-nego-needed", handleNegoNeedIncomming);
+    socket.on("peer-nego-final", handleNegoNeedFinal);
 
     return () => {
       socket.off('user-joined', handleUserJoined);
       socket.off('incoming-call', handleIncommingCall);
-
+      socket.off("call-accepted", handleCallAccepted);
+      socket.off("peer-nego-needed", handleNegoNeedIncomming);
+      socket.off("peer-nego-final", handleNegoNeedFinal);
     };
   }, [
     socket,
     handleUserJoined,
-    handleIncommingCall
+    handleIncommingCall,
+    handleCallAccepted,
+    handleNegoNeedIncomming,
+    handleNegoNeedFinal,
   ]);
 
-  useEffect(() => {
-    if (myVideoRef.current && myStream) {
-      myVideoRef.current.srcObject = myStream;
-    }
-  }, [myStream]);
 
   return (
     <div>
       <h1>Room Page</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
-      {/* {myStream && <button onClick={sendStreams}>Send Stream</button>} */}
+      {myStream && <button onClick={sendStreams}>Send Stream</button>}
       {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
       {myStream && (
         <>
@@ -82,18 +147,19 @@ function Room() {
           />
         </>
       )}
-      {/* {remoteStream && (
+      {remoteStream && (
         <>
           <h1>Remote Stream</h1>
           <ReactPlayer
             playing
-            muted
-            height="100px"
+            // muted
+            height="150px"
             width="200px"
             url={remoteStream}
+            style={{transform: "scaleX(-1)"}}
           />
         </>
-      )} */}
+      )}
     </div>
   );
 }
